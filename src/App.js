31f8f6e45e7b3ff4a0e3f205fef09d9e8736a95c
umit-turk <uyt_turk@hotmail.com>
2021-05-23 23:00:48 +0300

    logo and infoCards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from "react";
 import alanBtn from "@alan-ai/alan-sdk-web";
 import NewsCards from './components/NewsCards/NewsCards'
+import useStyles from './styles.js';
 
 const alanKey =
   "88e6e578e9b50da99deb99b9e22df65e2e956eca572e1d8b807a3e2338fdd0dc/stage";
 
 const App = () => {
+
   const [newsArticles, setNewsArticles] = useState([])
+  const classes = useStyles();
+
+
   useEffect(() => {
     alanBtn({
       key: alanKey,
@@ -19,7 +24,9 @@ const App = () => {
   }, []);
   return (
     <div>
-      <h1>Alan AI News Application</h1>
+      <div className={classes.logoContainer}>
+        <img src="http://summ.ai/wp-content/uploads/2019/09/logo-transparent-squared-25k-300x300.png" className={classes.alanLogo} alt="alan logo" />
+      </div>
       <NewsCards articles={newsArticles} /> {/* props olarak gönderdik. */}
     </div>
   );
