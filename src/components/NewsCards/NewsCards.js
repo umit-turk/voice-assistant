945692ab20e47b3801a5efb1d63b010435476c93
umit-turk <uyt_turk@hotmail.com>
2021-05-23 20:11:08 +0300

    arrange the NwesCard

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -1,15 +1,25 @@
-import React from 'react'
-import NewsCard from '../NewsCard/NewsCard'
+import React from "react";
+import { Grid, Grow, Typography } from "@material-ui/core";
+
+import useStyles from './styles.js'
+import NewsCard from "../NewsCard/NewsCard";
 
 //destructing yöntemi ile prop'u aldık ve map metodu ile tek tek yazdırdık.
-const NewsCards = ({articles}) => {
-    return (
-        <div>
-            {articles.map((article, i) => (
-                <NewsCard />
-            ))}
-        </div>
-    )
-}
+const NewsCards = ({ articles }) => {
 
-export default NewsCards
+    const classes = useStyles();
+console.log(articles)
+  return (
+    <Grow in>
+      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+        {articles.map((article, i) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} style={{display: 'flex'}}>
+                <NewsCard article={article} i={i} />
+            </Grid>
+        ))}
+      </Grid>
+    </Grow>
+  );
+};
+
+export default NewsCards;
